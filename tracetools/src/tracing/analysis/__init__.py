bd519710d6e78e924bc02a8f70af50fa5c6ca279
Luetkebohle Ingo (CR/AEX3) <ingo.luetkebohle@de.bosch.com>
2017-08-11 20:45:55 +0200

    handle action definitions in select

--- tracetools/src/tracing/analysis/__init__.py
+++ tracetools/src/tracing/analysis/__init__.py
@@ -20,15 +20,27 @@ def load_as_pd(filename):
     else:
         return ti2pd(map_roscpp(data))    
 
-def select_invocations(pd_df, fn_selector=lambda _: True, fields=("duration", "cycles")):
-    """Merges invocations and functions table, and returns all function invocations where the function_name 
-        is selected by the given selector function (default: all)"""
+def select_invocations(pd_df, fn_selector=lambda _: True):
+    """
+    Merges invocations and functions table, and returns all function invocations where the function_name 
+    is selected by the given selector function (default: all)
+    :return: List of dataframes, with either one element or multiple elements, if the input contains
+    action intervals (then, one per such interval)
+    """
     cbs = pd.Series(data=[index for index, row in pd_df.functions.\
                             iterrows() if fn_selector(row["function_name"])]).unique()
-    return pd_df.functions.loc[cbs].merge(pd_df.invocations, on='callback').\
-        loc[:,("function_name", ) + fields]
+    if len(pd_df.actions) > 0:
+        invs=[]
+        for name, a in pd_df.actions.iterrows():
+            start = a.starts
+            invocations = pd_df.invocations[start:start+a.duration]
+            invs.append(pd_df.functions.loc[cbs].merge(invocations, on='callback'))
+        return invs
+    return [pd_df.functions.loc[cbs].merge(pd_df.invocations, on='callback')]
 
-def fn_durations(fi_pd, stats=[np.sum, np.median, np.std], sort_key=("duration", "sum")):
+def fn_durations(fi_pd, stats=['count', np.sum, np.median, np.std], sort_key=("duration", "sum"), fields=("duration", "cycles")):
     """Compute median and standard deviation of aggregated invocation data."""
-    return fi_pd.groupby('function_name').agg(stats).\
+    return fi_pd.loc[:,("function_name", "trace_id") + fields].\
+        groupby(('function_name', 'trace_id')).agg(stats).\
         sort_values(by=sort_key, ascending=False)
+        
